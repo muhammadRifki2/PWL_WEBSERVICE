6259af645ee947409baf6136687cb854b3d4a4f7
muhammadRifki2 <muhammadrifki48@gmail.com>
2025-06-13 21:22:17 +0700

    done

---
+++ app/Controllers/Home.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel; 
+
+class Home extends BaseController
+{
+    protected $product;
+    function __construct()
+    {   
+        helper ('form');
+        helper ('number');
+        $this->product = new ProductModel();
+    }
+
+    public function index()
+    {
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+
+        return view('v_home', $data);
+    }
+
+    public function faq()
+    {
+        return view('v_faq'); // Mengarahkan ke view faq
+    }
+}
